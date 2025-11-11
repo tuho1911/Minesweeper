ee49c5105df7a583f0f22f53b56c1f1f0eeb646b
Nguyễn Thị Bình An <nguyenbinhan2707@gmail.com>
2025-11-11 23:01:51 +0700

    add CellButton & CellNumberColor

---
+++ Minesweeper/CellButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public class CellButton: Button
+    {
+        public int Row { get; set; } //Vị trí hàng
+        public int Col { get; set; } //Vị trí cột
+        public bool IsMine { get; set; } //Có phải mìn không
+        public bool IsRevealed { get; set; } //Đã được mở chưa
+        public bool IsFlagged { get; set; } //Đã được cắm cờ chưa
+        public int AdjacentMines { get; set; } //Số mìn xung quanh
+    }
+}
